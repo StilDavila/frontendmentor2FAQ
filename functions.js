e4a48e6921cb86f1ec72c2b719297b8250ec1f00
kevin <contact@kevin.com>
2020-10-13 23:20:22 -0500

    agregado js, falta parte desktop

--- functions.js
+++ functions.js
@@ -23,3 +23,26 @@ function cargarListeners(){
   });
 }
 */
+
+document.addEventListener("DOMContentLoaded", function() {
+  cargarListeners();
+});
+
+function cargarListeners(){
+  const $divQuestions = document.getElementsByClassName('form-question__question');
+  const $arrow = document.getElementById('arrow');
+  [...$divQuestions].forEach(divQuestion => {
+    divQuestion.childNodes[1].addEventListener('click', function(){
+      divQuestion.classList.toggle('active');
+      divQuestion.parentElement.childNodes[3].classList.toggle('hidden');
+      // console.log(divQuestion.classList.className);
+      divQuestion.childNodes[3].style.transform = "rotate(180deg)";
+
+      if(divQuestion.classList.contains('active')){
+        divQuestion.childNodes[3].style.transform = "rotate(180deg)";      
+      }else{
+        divQuestion.childNodes[3].style.transform = "rotate(-360deg)";
+      }
+    });
+  });
+}
