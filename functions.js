7d41bc4e47c872db443b626daab4fefffaf09bad
kevin <contact@kevin.com>
2020-10-13 22:07:11 -0500

    Terminado layout responsive, falta desktop y js

---
+++ functions.js
@@ -0,0 +1,25 @@
+/*
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  col
+
+document.addEventListener("DOMContentLoaded", function() {
+  cargarListeners();
+});
+
+function cargarListeners(){
+  const array = document.getElementsByClassName('form-question__question');
+  const questions = [...array];
+  questions.forEach(question => {
+    [...question.getElementsByTagName('span')].forEach(spanQuestion => {
+      spanQuestion.addEventListener('click',function(){
+        console.dir(`${question}`)
+      });
+    });
+    
+
+  });
+}
+*/
